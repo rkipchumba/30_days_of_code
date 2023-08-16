c6b73afc1645101347a5bd403fbcfadcf80d2045
rkipchumba <98005951+rkipchumba@users.noreply.github.com>
2023-08-16 14:29:59 +0300

    first commit

---
+++ 01_intro/helloworld.py
@@ -0,0 +1,12 @@
+print(3 // 2)
+print(3 % 2)
+
+# Checking data types
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Asabeneh'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name': 'Asabeneh'}))  # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
