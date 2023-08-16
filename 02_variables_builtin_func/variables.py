63af42617c0906a37756b0ac2af9f25cb250fed9
rkipchumba <98005951+rkipchumba@users.noreply.github.com>
2023-08-16 16:06:49 +0300

    added Operators

---
+++ 02_variables_builtin_func/variables.py
@@ -0,0 +1,14 @@
+first_name = 'Rodgers'
+last_name = 'Kipchumba'
+country = 'Kenya'
+age = 100
+skills = ['HTML', 'CSS', 'Py']
+personal_info = {
+    'firstname': 'Rodgers',
+    'lastname': 'Kipchumba',
+    'country': 'Kenya'
+}
+
+# Printing the values stored in the variables
+print('First Name: ', first_name)
+print('Personal Info: ', personal_info)
