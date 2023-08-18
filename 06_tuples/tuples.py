5e66ac7ac62102901e7b34a6c8735b25db040de9
rkipchumba <98005951+rkipchumba@users.noreply.github.com>
2023-08-18 18:00:50 +0300

    added tuples and sets

---
+++ 06_tuples/tuples.py
@@ -0,0 +1,9 @@
+# Syntax
+tpl = ('item1', 'item2', 'item3', 'item4')
+lst = list(tpl)
+fruits = ('banana', 'orange', 'mango', 'lemon')
+fruits = list(fruits)
+fruits[0] = 'apple'
+print(fruits)     # ['apple', 'orange', 'mango', 'lemon']
+fruits = tuple(fruits)
+print(fruits)     # ('apple', 'orange', 'mango', 'lemon')
